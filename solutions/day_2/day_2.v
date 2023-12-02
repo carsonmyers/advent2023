8c171ee8930b84c7ec3ee4233abd6150298692c2
Carson Myers <carson@myers.se>
2023-12-02 15:36:07 +0100

    completed day 2

--- solutions/day_2/day_2.v
+++ solutions/day_2/day_2.v
@@ -1,5 +1,8 @@
 module day_2
 
+import arrays
+import math
+
 pub fn run(input string, level int) !string {
 	if level == 1 {
 		return level_1(input)
@@ -9,9 +12,120 @@ pub fn run(input string, level int) !string {
 }
 
 fn level_1(input string) !string {
-	return error('not implemented')
+	limits := Sample{
+		red: 12
+		green: 13
+		blue: 14
+	}
+
+	lines := input.split('\n').filter(fn (line string) bool {
+		return line.len > 0
+	})
+
+	mut games := []Game{}
+	for line in lines {
+		game := parse_game(line)!
+		possible := game.samples.all(fn [limits] (sample Sample) bool {
+			return sample.red <= limits.red && sample.green <= limits.green
+				&& sample.blue <= limits.blue
+		})
+
+		if possible {
+			games << game
+		}
+	}
+
+	return arrays.sum(games.map(fn (game Game) int {
+		return game.number
+	}))!.str()
 }
 
 fn level_2(input string) !string {
-	return error('not implemented')
+	lines := input
+		.split('\n')
+		.filter(fn (line string) bool {
+			return line.len > 0
+		})
+
+	mut games := []Game{}
+	for line in lines {
+		games << parse_game(line)!
+	}
+
+	mut game_powers := []int{}
+	for game in games {
+		minimal_sample := arrays.reduce(game.samples, fn (acc Sample, sample Sample) Sample {
+			return Sample{
+				red: math.max(acc.red, sample.red)
+				green: math.max(acc.green, sample.green)
+				blue: math.max(acc.blue, sample.blue)
+			}
+		})!
+
+		game_powers << minimal_sample.power()
+	}
+
+	return arrays.sum(game_powers)!.str()
+}
+
+struct Game {
+pub:
+	number  int
+	samples []Sample
+}
+
+fn parse_game(line string) !Game {
+	parts := line.split(': ')
+	if parts.len != 2 {
+		return error('invalid game line: multiple `:` tokens found')
+	}
+
+	game_part := parts[0]
+	sample_part := parts[1]
+
+	game_parts := game_part.split(' ')
+	if game_parts.len != 2 {
+		return error('invalid game line: game name should be like `Game n`')
+	}
+
+	number := int(game_parts[1].parse_int(10, 32)!)
+
+	mut samples := []Sample{}
+	for sample in sample_part.split('; ') {
+		samples << parse_sample(sample)!
+	}
+
+	return Game{number, samples}
+}
+
+struct Sample {
+pub:
+	red   int
+	green int
+	blue  int
+}
+
+fn parse_sample(input string) !Sample {
+	groups := input.split(', ')
+
+	mut red := 0
+	mut green := 0
+	mut blue := 0
+	for group in groups {
+		group_parts := group.split(' ')
+		number := int(group_parts[0].parse_int(10, 32)!)
+		color := group_parts[1]
+		match color {
+			'red' { red = number }
+			'green' { green = number }
+			'blue' { blue = number }
+			else { return error('invalid colour ${color}') }
+		}
+	}
+
+	return Sample{red, green, blue}
+}
+
+fn (s Sample) power() int {
+	return s.red * s.green * s.blue
 }
--- solutions/day_2/day_2_test.v
+++ solutions/day_2/day_2_test.v
@@ -1,9 +1,25 @@
 module day_2
 
 fn test_level_1() {
-	assert false
+	input := 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+		Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+		Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+		Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+		Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
+		.replace('\t', '')
+
+	solution := level_1(input)!
+	assert solution == '8'
 }
 
 fn test_level_2() {
-	assert false
+	input := 'Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+		Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+		Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+		Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+		Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green'
+		.replace('\t', '')
+
+	solution := level_2(input)!
+	assert solution == '2286'
 }
