8d6c660251fe7fdbcfdcafc454ad6349b5067948
Carson Myers <carson@myers.se>
2023-12-09 16:06:35 +0100

    commented days 8 and 9

--- solutions/day_8/day_8.v
+++ solutions/day_8/day_8.v
@@ -11,20 +11,30 @@ pub fn run(input string, level int) !string {
 	}
 }
 
+// level_1 is a task to read a graph to find the number of steps between a
+// starting node and an ending node. Each node has two connections (right and
+// left) and the first line is a pattern of directions that must be taken when
+// navigating the graph. The starting position is a node named 'AAA' and the
+// destination is one named 'ZZZ'
 fn level_1(input string) !string {
 	lines := input.split('\n').filter(it.len > 0)
+
+	// read the directions pattern from the first line
 	mut directions := parse_directions(lines[0])!
 
+	// read all of the nodes into a map to easily look them up as we follow
+	// the directions in search of the end node
 	mut nodes := map[string]Node{}
 	for i in 1 .. lines.len {
 		node := parse_node(lines[i])!
 		nodes[node.id] = node
 	}
 
+	// ensure we read a valid set of data; this doesn't guarantee that the
+	// search algorithm terminates but should catch any obvious mistakes
 	if nodes.len == 0 {
 		return error('no nodes read')
 	}
-
 	if 'AAA' !in nodes {
 		return error('no start node')
 	}
@@ -32,25 +42,38 @@ fn level_1(input string) !string {
 		return error('no end node')
 	}
 
+	// start at the start node and follow the graph according to the directions
+	// until the final node is reached
 	mut current := nodes['AAA']
 	mut steps := 0
 	for {
+		// we made it!
 		if current.id == 'ZZZ' {
 			return steps.str()
 		}
 
+		// get the next direction and read the left-or-right link to the next
+		// node in the graph. Load that node and begin the next step
 		dir := directions.next() or { panic('no next direction') }
 		current = nodes[current.get(dir)]!
 		steps += 1
 	}
 
+	// the above loop can only terminate by returning or panicking but the
+	// compiler still requires a return statement here for some reason
 	return error('nothing found')
 }
 
+// level_2 steps up the challenge by designating all nodes whose name ends in A
+// as start nodes, and all that end in Z as end nodes. This time ghosts are
+// following the map (??) by following the start nodes to the end nodes
+// simultaneously. The task is only complete when all paths have reached an
+// end node on at the same time.
 fn level_2(input string) !string {
 	lines := input.split('\n').filter(it.len > 0)
 	mut directions := parse_directions(lines[0])!
 
+	// load all the nodes into a map and keep track of the start nodes
 	mut nodes := map[string]Node{}
 	mut start_nodes := []string{}
 	for i in 1 .. lines.len {
@@ -62,6 +85,7 @@ fn level_2(input string) !string {
 		}
 	}
 
+	// sanity check
 	if nodes.len == 0 {
 		return error('no nodes read')
 	}
@@ -69,16 +93,24 @@ fn level_2(input string) !string {
 		return error('no start nodes')
 	}
 
+	// start searching from each start node in a separate thread
 	res := chan i64{}
 	for id in start_nodes {
 		go search_for_end(id, nodes, directions, res)
 	}
 
+	// collect all the shortest paths from start to end nodes through a
+	// channel. Note that while there are probably shorter paths which can
+	// be found by deviating from the directions, this just accounts for the
+	// path length without having to go past the end node because every
+	// path hasn't reached their end node together at the same time.
 	mut shortest_paths := []i64{}
 	for _ in 0 .. start_nodes.len {
 		shortest_paths << <-res
 	}
 
+	// find the least common multiple of all the path lengths to determine how
+	// many steps it will take for all of them to reach an end node at once
 	result := arrays.reduce(shortest_paths, fn (a i64, b i64) i64 {
 		gcd := greatest_common_divisor(a, b)
 		return (a * b) / gcd
@@ -87,6 +119,7 @@ fn level_2(input string) !string {
 	return result.str()
 }
 
+// greatest_common_divisor is the largest number which can divide both numbers
 fn greatest_common_divisor(a i64, b i64) i64 {
 	if a == 0 {
 		return b
@@ -95,6 +128,10 @@ fn greatest_common_divisor(a i64, b i64) i64 {
 	return greatest_common_divisor(b % a, a)
 }
 
+// search_for_end is an algorithm which navigates the input nodes for an end
+// node (ending in 'Z') by following the given directions. Once the end node
+// is found, the number of steps it took to reach it is reported on the
+// res channel.
 fn search_for_end(start string, nodes map[string]Node, directions &Directions, res chan i64) {
 	mut steps := i64(0)
 	mut current := nodes[start]
@@ -111,12 +148,16 @@ fn search_for_end(start string, nodes map[string]Node, directions &Directions, r
 	}
 }
 
+// Directions is an infinitely-repeating iterator of left-and-right instructions
+// to be followed while traversing the input map
 struct Directions {
 	directions []Direction
 mut:
 	idx int
 }
 
+// parse_directions read a list of L and R characters from the input and uses
+// them to create a Directions struct
 fn parse_directions(input string) !Directions {
 	mut directions := []Direction{}
 	for c in input {
@@ -133,6 +174,9 @@ fn parse_directions(input string) !Directions {
 	}
 }
 
+// next produces the next instruction for reading the map. If the number of
+// instructions from the input have been exhausted, wrap back around to the
+// beginning
 fn (mut d Directions) next() ?Direction {
 	if d.directions.len == 0 {
 		return none
@@ -147,6 +191,7 @@ fn (mut d Directions) next() ?Direction {
 	return res
 }
 
+// clone is needed to simplify passing the instructions to a thread
 fn (d &Directions) clone() Directions {
 	return Directions{
 		directions: d.directions.clone()
@@ -159,12 +204,20 @@ enum Direction {
 	right
 }
 
+// Node is a node in the map, which has a name and two connections (left and
+// right) to other nodes by name
 struct Node {
 	id    string
 	left  string
 	right string
 }
 
+// parse_node reads a node from the input. It is specified like:
+//
+// AAA = (BBB, CCC)
+//
+// where AAA is the node name, BBB is the 'left' node's name, and CCC is the
+// 'right' node's name.
 fn parse_node(input string) !Node {
 	pattern := pcre.new_regex(r'^(\w+) = \((\w+), (\w+)\)$', 0)!
 	m := pattern.match_str(input, 0, 0) or { return error('malformed node: "${input}"') }
--- solutions/day_9/day_9.v
+++ solutions/day_9/day_9.v
@@ -10,6 +10,8 @@ pub fn run(input string, level int) !string {
 	}
 }
 
+// level_1 is to read historical lists of values and predict the next number
+// in each sequence. The solution is the sum of all of the next numbers.
 fn level_1(input string) !string {
 	mut next_nums := []i64{}
 	for line in input.split('\n').filter(it.len > 0) {
@@ -20,6 +22,8 @@ fn level_1(input string) !string {
 	return arrays.sum(next_nums)!.str()
 }
 
+// level_2 is similar to the previous challenge, except it is to predict the
+// preceding value of each sequence.
 fn level_2(input string) !string {
 	mut prev_nums := []i64{}
 	for line in input.split('\n').filter(it.len > 0) {
@@ -30,8 +34,12 @@ fn level_2(input string) !string {
 	return arrays.sum(prev_nums)!.str()
 }
 
+// parse_numbers reads a list of single-space-separated numbers from the
+// input. Numbers are allowed to be negative.
 fn parse_numbers(input string) ![]i64 {
 	mut nums := []i64{}
+
+	// no more than a single space separates each number
 	for num_str in input.split(' ') {
 		nums << num_str.parse_int(10, 64)!
 	}
@@ -39,44 +47,95 @@ fn parse_numbers(input string) ![]i64 {
 	return nums
 }
 
+// find_next_in_seq predicts the next value in a sequence by calculating each
+// sequence's derivative until a constant function is found. That information
+// is used to determine how much needs to be added to the sequence to find
+// the next step.
+//
+// for the sequence 0 3 6 9 12 15:
+// the difference between each step is found: 3 3 3 3 3
+// the process is repeated: 0 0 0 0
+//
+// once all the values are zeroes, the next number can be calculated by summing
+// the rightmost number of each sequence (the input as well as each computed
+// sequence).
 fn find_next_in_seq(numbers []i64) !i64 {
+	// store the last value of each sequence
 	mut ends := [numbers[numbers.len - 1]]
+
+	// store the current sequence being derived
 	mut step := numbers.clone()
+
 	for {
+		// derive the current step and add the last digit
 		derivative := derive(step)
 		ends << derivative[derivative.len - 1]
+
+		// once the function has been reduced to a constant, we're done
 		if is_constant(derivative) {
 			break
 		}
 
+		// set the derivative to the next step to repeat the process
 		unsafe {
 			step = derivative
 		}
 	}
 
+	// the sum of the last digits is the next number in the input sequence
 	return arrays.sum(ends)!
 }
 
+// find_prev_in_seq predicts the previous value in a sequence by calculating
+// each sequence's derivative until a constant function is found. That
+// information is used to determine how much needs to be subtracted from the
+// sequence to find the previous step.
+//
+// for the sequence 10 13 16 21 30 45:
+// the difference between each step is found: 3 3 5 9 15
+// the process is repeated: 0 2 4 6
+// and again: 2 2 2
+// and again: 0 0
+//
+// once all the values are zeroes, the previous number can be calculated by
+// subtracting the first number of each sequence from each other:
+//
+// 0 2 0 3 10 => 2 - 0 = 2
+// 				 0 - 2 = -2
+// 				 3 - -2 = 5
+// 				 10 - 5 = 5
+//
+// so the previous digit is 5
 fn find_prev_in_seq(numbers []i64) !i64 {
+	// store the beginning number of each sequence
 	mut starts := [numbers[0]]
+
+	// store the current sequence being derived
 	mut step := numbers.clone()
 	for {
+		// derive the current step and add the first number
 		derivative := derive(step)
 		starts << derivative[0]
+
+		// once the function has been reduced to constant, we're done
 		if is_constant(derivative) {
 			break
 		}
 
+		// set the derivative to the next step to continue the process
 		unsafe {
 			step = derivative
 		}
 	}
 
+	// subtract pairs of the first numbers from each other to find the
+	// preceding value of the input sequence
 	return arrays.reduce(starts.reverse(), fn (a i64, b i64) i64 {
 		return b - a
 	})!
 }
 
+// derive an input sequence by calculating the distance between each number
 fn derive(numbers []i64) []i64 {
 	mut res := []i64{}
 	for i in 1 .. numbers.len {
@@ -86,6 +145,8 @@ fn derive(numbers []i64) []i64 {
 	return res
 }
 
+// is_constant checks if the sequence is the derivative of a constant function
+// (all the numbers are 0)
 fn is_constant(numbers []i64) bool {
 	return numbers.all(it == 0)
 }
